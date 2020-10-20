a894472207eca93773a3402475325f894a2fbe73
naveeny061 <naveeny061@gmail.com>
2020-10-20 15:03:51 +0530

    adding 3rd compute

--- sortArthematic.sh
+++ sortArthematic.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash
 echo "Welcome To Arithmetic Computation & Sorting Program"
 read -p "enter input 1st " a
 read -p "enter input 2nd " b
@@ -7,3 +7,5 @@ result1=$((a+b*c))
 echo $result1
 result2=$((a*b+c))
 echo $result2
+result3=$( echo | awk '{print three+one/two}' one=$a two=$b three=$c )
+echo $result3
