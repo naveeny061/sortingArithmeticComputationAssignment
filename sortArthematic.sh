a1b1bd6e0ab45d4fa191e2f64e334c266a16d5e8
naveeny061 <naveeny061@gmail.com>
2020-10-20 14:45:00 +0530

    computing a b c

--- sortArthematic.sh
+++ sortArthematic.sh
@@ -3,4 +3,5 @@ echo "Welcome To Arithmetic Computation & Sorting Program"
 read -p "enter input 1st " a
 read -p "enter input 2nd " b
 read -p "enter input 3rd " c
-
+result=$((a+b*c))
+echo $result
