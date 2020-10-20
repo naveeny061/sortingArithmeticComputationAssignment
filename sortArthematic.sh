c7272e0dbf25a02fe6f5c9fa90ca43365e78cb2b
naveeny061 <naveeny061@gmail.com>
2020-10-20 16:00:38 +0530

    adding dictionary values to array

--- sortArthematic.sh
+++ sortArthematic.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -x
 declare -A result
+declare -a resultArray
 echo "Welcome To Arithmetic Computation & Sorting Program"
 read -p "enter input 1st " a
 read -p "enter input 2nd " b
@@ -9,3 +10,10 @@ result[2]=$((a*b+c))
 result[3]=$( echo | awk '{print three+one/two}' one=$a two=$b three=$c )
 result[4]=$( echo | awk '{print one%two+three}' one=$a two=$b three=$c )
 echo "dictionary is "${result[@]}
+loop=1
+while [ $loop -lt 5 ]
+do
+resultArray[$loop]=${result[$loop]}
+((loop++))
+done
+echo "array is" ${resultArray[@]}
