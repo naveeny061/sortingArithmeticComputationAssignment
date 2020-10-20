aa8e8ca319f93712cee11e6363c008122b3755f6
naveeny061 <naveeny061@gmail.com>
2020-10-20 14:53:47 +0530

    adding 2nd compute

--- sortArthematic.sh
+++ sortArthematic.sh
@@ -3,5 +3,7 @@ echo "Welcome To Arithmetic Computation & Sorting Program"
 read -p "enter input 1st " a
 read -p "enter input 2nd " b
 read -p "enter input 3rd " c
-result=$((a+b*c))
-echo $result
+result1=$((a+b*c))
+echo $result1
+result2=$((a*b+c))
+echo $result2
